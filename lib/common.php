c7bbbd8dc73bb1e5268dfc8d6ae74e19d5a28537
root <moises@organic.com.pa>
2015-07-29 22:19:56 +0000

    Adjustment.

--- lib/common.php
+++ lib/common.php
@@ -15,7 +15,7 @@ if ($CFG->memcached) {
 	$CFG->m = new Memcached();
 	$CFG->m->addServer('localhost',11211);
 	$CFG->m->setOption(Memcached::OPT_COMPRESSION,false);
-	//$CFG->m->setOption(Memcached::OPT_BINARY_PROTOCOL,true);
+	$CFG->m->setOption(Memcached::OPT_BINARY_PROTOCOL,true);
 }
 
 /* Load settings and timezone */
